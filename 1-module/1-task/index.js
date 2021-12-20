b90efcf19abbc2028f7b075dea2d0a3ed9720453
Marina Titenko <mr.marish@gmail.com>
2021-12-20 15:16:30 +0100

    Решение задачи 2

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+
+  if (n == 1 || n == 0) {
+    return result;
+  } else {
+    let i = n;
+    while (i >= 1) {
+      result = result * i;
+      i--;
+    }
+    return result;
+  }
 }
