e4ef52c1a96f0023f87a96ba489c7b7701e3360a
Marina Titenko <mr.marish@gmail.com>
2022-01-10 16:16:48 +0100

    Модуль 3 задачи 1-5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,21 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str
+    .split(" ")
+    .map(function (item) {
+      return Number(item);
+    })
+    .filter((item) => !isNaN(item));
+
+  let max = Number.MIN_VALUE;
+  let min = Number.MAX_VALUE;
+
+  for (let item of arr) {
+    if (item >= max) {
+      max = item;
+    }
+    if (item <= min) {
+      min = item;
+    }
+  }
+  return { min: min, max: max };
 }
