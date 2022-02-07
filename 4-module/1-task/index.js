cea363fda73b1f9dfc4de2c2ca2045d4d35835c2
Marina Titenko <mr.marish@gmail.com>
2022-02-07 16:59:55 +0100

    Модуль 4 задачи 1-3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,19 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("ul");
+
+  let users = friends
+    .map((item) => item.firstName + " " + item.lastName)
+    .map((item) => {
+      let li = document.createElement("li");
+      li.innerHTML = item;
+      return li;
+    });
+
+  users.forEach((item) => {
+    ul.appendChild(item);
+  });
+  return ul;
+
+  // console.log(users);
+  //   console.log(ul)
 }
